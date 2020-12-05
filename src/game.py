9f1db8cd489ea6076633e2c60f7fb5e89797e1d6
m6ttw <matthewrhyswilliams@live.com>
2020-12-05 13:24:54 +0000

    made game into a class, reverted controller.py

--- src/game.py
+++ src/game.py
@@ -1,14 +1,18 @@
-def get_winner(player_1, player_2):
-    if player_1.choice == "rock" and player_2.choice == "scissors":
-        return player_1
+from src.player import Player
 
-    if player_1.choice == "scissors" and player_2.choice == "paper":
-        return player_1
+class Game():
 
-    if player_1.choice == "paper" and player_2.choice == "rock":
-        return player_1
+    def get_winner(player_1, player_2):
+        if player_1.choice == "rock" and player_2.choice == "scissors":
+            return player_1
 
-    if player_1.choice == player_2.choice:
-        return None
+        if player_1.choice == "scissors" and player_2.choice == "paper":
+            return player_1
 
-    return player_2
+        if player_1.choice == "paper" and player_2.choice == "rock":
+            return player_1
+
+        if player_1.choice == player_2.choice:
+            return None
+
+        return player_2
--- tests/player_test.py
+++ tests/player_test.py
@@ -4,10 +4,10 @@ from src.player import Player
 class TestPlayer(unittest.TestCase):
 
     def setUp(self):
-        self.player = Player("Player", "Rock")
+        self.player = Player("Player", "rock")
 
     def test_player_has_name(self):
         self.assertEqual("Player", self.player.name)
 
     def test_player_has_choice(self):
-        self.assertEqual("Rock", self.player.choice)
+        self.assertEqual("rock", self.player.choice)
