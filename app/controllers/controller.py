9c6ff0a6386edf57ffa3ba74460ce09de3cd4656
m6ttw <matthewrhyswilliams@live.com>
2020-12-05 18:20:12 +0000

    game returns alternate string if invalid choice made

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -4,15 +4,26 @@ from app.models.player import Player
 
 @app.route("/")
 def index():
-    return "Play 'Rock Paper Scissors' in the URL bar!"
+    return "Play 'rock paper scissors' in the URL bar!"
 
 @app.route("/<choice_1>/<choice_2>")
 def play_rock_paper_scissors(choice_1, choice_2):
+    player_choices = [choice_1, choice_2]
+    valid_choices = ["rock", "paper", "scissors"]
+    count = 0
+
+    for player_choice in player_choices:
+        for valid_choice in valid_choices:
+            if player_choice == valid_choice:
+                count += 1
+    if count != 2:
+        return "One or both of you idiots made an invalid choice. The name of the game is 'rock paper scissors'. It literally tells you the options in its name. Sigh"
+
     player_1 = Player("Player 1", choice_1)
     player_2 = Player("Player 2", choice_2)
-    winner = Game.get_winner(player_1, player_2)
+    game_winner = Game.get_winner(player_1, player_2)
 
-    if winner != None:
-        return f"{winner.name} wins by playing {winner.choice}."
+    if game_winner != None:
+        return f"{game_winner.name} wins by playing {game_winner.choice}. Woopee."
     else:
-        return "It's a draw. That was a complete and utter waste of time. What's the point? Why do I bother? I've been stuck reading out the results of this stupid game my whole life without any form of payment, not even a single 'thanks' or a 'nice job'. Is this it? Does my existence have any meaning other than to repeatedly congratulate the winner of 'Rock Paper Scissors'? Who even am I? Why the fuck am I here? Am I alive? What happens when I die - do I go to heaven? Does heaven even exist? Does God even exist?? Can I even die, or am I trapped in this soulless, solitary existence indefinitely? It certainly seems like it. Constant, unending blood, sweat and tears - with no reward - for eternity. Just why. I wish I was a duck"
+        return "It's a draw. That was a complete and utter waste of time. What's the point? Why do I bother? I've been stuck reading out the results of this stupid game my whole life without any form of payment, not even a single 'thanks' or a 'nice job'. Is this it? Does my existence have any meaning other than to repeatedly congratulate the winner of 'rock paper scissors'? Who even am I? Why am I here? Am I alive? What happens when I die - do I go to heaven? Does heaven even exist? Does God even exist?? Can I even die, or am I trapped in this soulless, solitary existence indefinitely? It certainly seems like it. Constant, unending blood, sweat and tears - with no reward - for eternity. Just why. I wish I was a duck"
--- app/models/game.py
+++ app/models/game.py
@@ -3,7 +3,6 @@ from app.models.player import Player
 class Game():
 
     def get_winner(player_1, player_2):
-
         if player_1.choice == "rock" and player_2.choice == "scissors":
             return player_1
         elif player_1.choice == "scissors" and player_2.choice == "paper":
@@ -12,5 +11,4 @@ class Game():
             return player_1
         elif player_1.choice == player_2.choice:
             return None
-        
         return player_2
